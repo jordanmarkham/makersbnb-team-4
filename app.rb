c760f67e86cfb4e45ea702f8901d989d8f22befa
HanzAkor <95639001+HanzAkor@users.noreply.github.com>
2022-07-21 10:36:14 +0100

    App controller updated

--- app.rb
+++ app.rb
@@ -3,6 +3,7 @@
 require 'sinatra/base'
 require 'sinatra/reloader'
 require_relative 'lib/space'
+require_relative 'lib/user'
 
 class MakersBnb < Sinatra::Base
   enable :sessions
@@ -15,27 +16,63 @@ class MakersBnb < Sinatra::Base
     'Test page'
   end
 
-  get '/login' do
-    erb(:login)
-  end
-
   get '/' do
-    @all_spaces = Space.all
-    erb(:index)
+    session[:logged_in?] == true ? redirect('/spaces') : erb(:index)
   end
 
-  get '/new' do
-    erb(:new)
-  end
-
-  post '/new' do
-    erb(:new)
-  end
-
-  post '/' do
-    Space.create(params[:name], params[:description], params[:price_per_night])
+  get '/signup' do
     redirect('/')
   end
 
+  post '/signup' do
+    User.signup(params[:username], params[:firstname], params[:lastname], params[:email], params[:password])
+    login(params[:username], params[:email], params[:password])
+    redirect('/spaces?signup=success')
+  end
+
+  post '/login' do
+    session[:logged_in?] = true
+  end
+
+  post '/logout' do
+    logout
+    redirect('/spaces')
+  end
+
+  get '/spaces' do
+    @success = params['signup']
+    @all_spaces = Space.all
+    erb(:spaces)
+  end
+
+  get '/spaces/new' do
+    session[:logged_in?] == true ? erb(:new) : redirect('/')
+  end
+
+  post '/spaces/new' do
+    session[:logged_in?] == true ? erb(:new) : redirect('/')
+  end
+
+  post '/create' do
+    Space.create(params[:name], params[:description], params[:price_per_night], session[:username])
+    redirect('/spaces')
+  end
+
+  def login(username, email, password)
+    session[:username] = username
+    session[:email] = email
+    session[:password] = password
+    session[:logged_in?] = true
+  end
+
+  def logout
+    session[:username] = nil
+    session[:email] = nil
+    session[:password] = nil
+    session[:logged_in?] = false
+  end
+
+
   run! if app_file == $PROGRAM_NAME
+
 end
